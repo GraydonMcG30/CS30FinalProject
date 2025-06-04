35532934e97d5909e3ca264169d5362a3cf6e2cc
GraydonMcG30 <graydon.mcgovern@rbe.sk.ca>
2025-06-04 09:30:49 -0600

    Updated ocean map gen
    Adding random obstacles tmrw

--- Ocean.py
+++ Ocean.py
@@ -1,1 +1,19 @@
-class Ocean
+class Ocean:
+    def __init__ (self, height, width):
+        self.width = width
+        self.height = height
+        self.map = self.makeMap()
+    def makeMap(self):
+        map_ = []
+        for row in range(self.height):
+            map_.append([])
+            for column in range(self.width):
+                map_[row].append([])
+        return map_
+    
+oceanMap = Ocean(10, 10)
+print(oceanMap.map)
+                
+        
+        
+        
