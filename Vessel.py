76cea0e6d94ab2ded526a28637b4cde3c498439a
GraydonMcG30 <graydon.mcgovern@rbe.sk.ca>
2025-06-09 09:31:39 -0600

    Started work on shot processing logic
    Created parameters for shooting and started working on turn processor

--- Vessel.py
+++ Vessel.py
@@ -1,6 +1,6 @@
-
-
-
+#Imports and Global Variables
+import turnProcessor
+    
 #Classes
 class Vessel:
     """Any and all ships."""
@@ -21,9 +21,23 @@ class Vessel:
         if self.health <= 0:
             self.exists = False
         
-    def move(xshift, yshift):
+    def move(self, xshift, yshift):
         self.x = x + xshift
         self.y = y + yshift
     
+    def fire(self):
+        while True:
+            coord1 = int(input("Please input the target column."+
+                                      "Must be within 5 tiles.")) - 1
+            coord2 = int(input("Please input the target column."+
+                                      "Must be within 5 tiles.")) - 1
+            if coord1 > self.x - 6 and coord1 < self.x + 6 and coord2 > self.y - 6 and coord2 < self.y + 6:
+                newTuple = (coord1, coord2)
+                turnProcessor.targets.add(newTuple)
+                break
+            else:
+                print("Out of range.")
+            
+        
 
     
---
+++ turnProcessor.py
@@ -0,0 +1,8 @@
+#Imports and Global Variables
+
+shipList = []
+targets = set()
+
+def checkShots:
+    for target in targets:
+        
